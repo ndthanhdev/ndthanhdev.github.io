004b2b008d8325ca27b22431b0b53be4643daba5
ndthanhdev <ndthanhdev@outlook.com>
2022-04-14 01:47:45 +0700

    add GA

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,40 +1,70 @@
 /** @type {import('gatsby').GatsbyConfig} */
 module.exports = {
-  siteMetadata: {
-    title: `new`,
-    siteUrl: `https://www.yourdomain.tld`,
-  },
-  plugins: [
-    "gatsby-plugin-layout",
-    "gatsby-plugin-sass",
-    "gatsby-plugin-theme-ui",
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/icon.png",
-      },
-    },
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "pages",
-        path: "./src/pages/",
-      },
-      __key: "pages",
-    },
-  ],
+	siteMetadata: {
+		title: `new`,
+		siteUrl: `https://ndthanhdev.github.io`,
+	},
+	plugins: [
+		"gatsby-plugin-layout",
+		"gatsby-plugin-sass",
+		"gatsby-plugin-theme-ui",
+		"gatsby-plugin-image",
+		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-sitemap",
+		{
+			resolve: "gatsby-plugin-manifest",
+			options: {
+				icon: "src/images/icon.png",
+			},
+		},
+		"gatsby-plugin-mdx",
+		"gatsby-plugin-sharp",
+		"gatsby-transformer-sharp",
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "images",
+				path: "./src/images/",
+			},
+			__key: "images",
+		},
+		{
+			resolve: "gatsby-source-filesystem",
+			options: {
+				name: "pages",
+				path: "./src/pages/",
+			},
+			__key: "pages",
+		},
+		{
+			resolve: `gatsby-plugin-google-gtag`,
+			options: {
+				// You can add multiple tracking ids and a pageview event will be fired for all of them.
+				trackingIds: [
+					"G-HT08YR5ZW7",
+					// "GA-TRACKING_ID", // Google Analytics / GA
+					// "AW-CONVERSION_ID", // Google Ads / Adwords / AW
+					// "DC-FLOODIGHT_ID", // Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+				],
+				// This object gets passed directly to the gtag config command
+				// This config will be shared across all trackingIds
+				gtagConfig: {
+					optimize_id: "OPT_CONTAINER_ID",
+					anonymize_ip: true,
+					cookie_expires: 0,
+				},
+				// This object is used for configuration specific to this plugin
+				pluginConfig: {
+					// Puts tracking script in the head instead of the body
+					head: false,
+					// Setting this parameter is also optional
+					respectDNT: true,
+					// Avoids sending pageview hits from custom paths
+					// exclude: ["/preview/**", "/do-not-track/me/too/"],
+					// Defaults to https://www.googletagmanager.com
+					// origin: "YOUR_SELF_HOSTED_ORIGIN",
+				},
+			},
+		},
+	],
 };
--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -3,12 +3,11 @@ import { ThemeProvider, merge, Themed } from "theme-ui";
 import { Helmet } from "react-helmet";
 
 let Layout: React.FC<{}> = function Layout(props) {
-  return (
-    <>
-      <Helmet>{/*  */}</Helmet>
-      <div>{props.children}</div>
-    </>
-  );
+	return (
+		<>
+			<div>{props.children}</div>
+		</>
+	);
 };
 
 export default Layout;
