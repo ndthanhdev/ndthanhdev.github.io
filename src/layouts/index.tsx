9737b82871e7dda2fb6bab7bae6ffe751fafcaa6
ndthanhdev <ndthanhdev@outlook.com>
2022-04-26 22:23:20 +0700

    Auto navigate

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { ThemeProvider, merge, Themed } from "theme-ui";
+import { ThemeProvider, merge } from "theme-ui";
 import { Helmet } from "react-helmet";
 
-let Layout: React.FC<{}> = function Layout(props) {
+let Layout: React.FC<{
+	children: React.ReactNode;
+}> = (props) => {
 	return (
 		<>
 			<div>{props.children}</div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,40 @@
 import * as React from "react";
+import { Link, navigate } from "gatsby";
 
 function Index() {
-  return <span>Under construction</span>;
+	let [count, decrease] = React.useReducer(
+		(state: number, _action: undefined) => {
+			return state - 1;
+		},
+		5
+	);
+
+	count = Math.max(count, 0);
+
+	let handleTick = () => {
+		if (count <= 0) {
+			navigate("/cv");
+		}
+		decrease(undefined);
+	};
+
+	const handleTickRef = React.useRef(handleTick);
+	handleTickRef.current = handleTick;
+
+	React.useEffect(() => {
+		let interval = setInterval(() => handleTickRef.current(), 1000);
+
+		return () => clearInterval(interval);
+	}, []);
+
+	return (
+		<>
+			<h2>
+				I have not built the homepage yet 😁, navigating to{" "}
+				<Link to="/cv">my CV</Link> in {count} seconds...
+			</h2>
+		</>
+	);
 }
 
 export default Index;
