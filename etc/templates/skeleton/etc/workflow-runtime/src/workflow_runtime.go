bbfa115013f2246081f76551289fee7ef1c80692
Duy Thanh Nguyen <16331812+ndthanhdev@users.noreply.github.com>
2025-06-22 17:07:23 +0700

    chore: fix release (#72)

--- etc/templates/skeleton/etc/workflow-runtime/src/workflow_runtime.go
+++ etc/templates/skeleton/etc/workflow-runtime/src/workflow_runtime.go
@@ -1,10 +1,80 @@
 package main
 
-import "dagger/workflow_runtime/internal/dagger"
+import (
+	"context"
+	"dagger/workflow_runtime/internal/dagger"
+	"fmt"
+)
 
 type WorkflowRuntime struct {
-	Dir     *dagger.Directory
-	Mode    string
-	GhToken *dagger.Secret
-	Con     *dagger.Container
+	Dir *dagger.Directory
+	Con *dagger.Container
+}
+
+// Setup Proto
+func (m *WorkflowRuntime) BuildBaseEnv(ctx context.Context) *WorkflowRuntime {
+	m.Con = dag.
+		Container().
+		From("ubuntu:plucky").
+		// apt-get update && apt-get install -y curl git unzip gzip xz-utils
+		WithExec([]string{"apt-get", "update"}).
+		WithExec([]string{"apt-get", "install", "-y", "build-essential", "curl", "git", "unzip", "bash", "gzip", "xz-utils"}).
+		// curl -fsSL https://moonrepo.dev/install/proto.sh | bash -s 0.35.3 --yes
+		WithExec([]string{"bash", "-l", "-c", "curl -fsSL https://moonrepo.dev/install/proto.sh | bash -s 0.44.1 --yes"}).
+		WithEnvVariable("PROTO_HOME", "/root/.proto", dagger.ContainerWithEnvVariableOpts{Expand: true}).
+		WithEnvVariable("PATH", "$PATH:$PROTO_HOME/shims:$PROTO_HOME/bin", dagger.ContainerWithEnvVariableOpts{Expand: true})
+
+	return m
+}
+
+func (m *WorkflowRuntime) BuildEnv(ctx context.Context) *WorkflowRuntime {
+	source := dag.Directory().WithDirectory("/", m.Dir, dagger.DirectoryWithDirectoryOpts{
+		Exclude: []string{"node_modules", ".cache", ".moon/cache"},
+	})
+
+	m.Con = m.BuildBaseEnv(ctx).
+		Con.
+		WithWorkdir("/workspace").
+		WithFile("/workspace/.prototools", m.Dir.File(".prototools")).
+		// proto use
+		WithExec([]string{"proto", "use"}).
+		// rm .prototools
+		WithoutFile("/workspace/.prototools").
+		WithMountedDirectory("/workspace", source).
+		// moon setup
+		WithExec([]string{"moon", "setup"}).
+		// yarn install --immutable
+		WithExec([]string{"yarn", "install", "--immutable"})
+
+	return m
+}
+
+func (m *WorkflowRuntime) WithAction(ctx context.Context, action string) *WorkflowRuntime {
+	m.Con = m.Con.WithExec([]string{"bash", "-l", "-c", fmt.Sprintf("./etc/scripts/actions/%s.ts", action)})
+
+	return m
+}
+
+func (m *WorkflowRuntime) Container(ctx context.Context) *dagger.Container {
+	return m.Con
+}
+
+func (m *WorkflowRuntime) Stdout(ctx context.Context) (string, error) {
+	stdout, err := m.Con.Stdout(ctx)
+	if err != nil {
+		return "", fmt.Errorf("failed to get stdout: %w", err)
+	}
+	return stdout, nil
+}
+
+func (m *WorkflowRuntime) WithEnvVariable(name, value string) *WorkflowRuntime {
+	m.Con = m.Con.WithEnvVariable(name, value)
+
+	return m
+}
+
+func (m *WorkflowRuntime) WithSecretVariable(name string, secret *dagger.Secret) *WorkflowRuntime {
+	m.Con = m.Con.WithSecretVariable(name, secret)
+
+	return m
 }
--- etc/workflow-runtime/src/main.go
+++ etc/workflow-runtime/src/main.go
@@ -15,96 +15,15 @@
 package main
 
 import (
-	"context"
 	"dagger/workflow_runtime/internal/dagger"
-	"fmt"
 )
 
 func New(
 	// +required
 	// +ignore=["node_modules/", ".moon/cache/", ".cache/"]
 	dir *dagger.Directory,
-	// +optional
-	// +default="dev"
-	mode string,
-	// +optional
-	ghToken *dagger.Secret,
 ) *WorkflowRuntime {
 	return &WorkflowRuntime{
-		Dir:     dir,
-		Mode:    mode,
-		GhToken: ghToken,
+		Dir: dir,
 	}
-}
-
-// Setup Proto
-func (m *WorkflowRuntime) BuildBaseEnv(ctx context.Context) *WorkflowRuntime {
-	m.Con = dag.
-		Container().
-		From("ubuntu:plucky").
-		// apt-get update && apt-get install -y curl git unzip gzip xz-utils
-		WithExec([]string{"apt-get", "update"}).
-		WithExec([]string{"apt-get", "install", "-y", "build-essential", "curl", "git", "unzip", "bash", "gzip", "xz-utils"}).
-		// curl -fsSL https://moonrepo.dev/install/proto.sh | bash -s 0.35.3 --yes
-		WithExec([]string{"bash", "-l", "-c", "curl -fsSL https://moonrepo.dev/install/proto.sh | bash -s 0.44.1 --yes"}).
-		WithEnvVariable("PROTO_HOME", "/root/.proto", dagger.ContainerWithEnvVariableOpts{Expand: true}).
-		WithEnvVariable("PATH", "$PATH:$PROTO_HOME/shims:$PROTO_HOME/bin", dagger.ContainerWithEnvVariableOpts{Expand: true})
-
-	return m
-}
-
-func (m *WorkflowRuntime) BuildEnv(ctx context.Context) *WorkflowRuntime {
-	source := dag.Directory().WithDirectory("/", m.Dir, dagger.DirectoryWithDirectoryOpts{
-		Exclude: []string{"node_modules", ".cache", ".moon/cache"},
-	})
-
-	m.Con = m.BuildBaseEnv(ctx).
-		Con.
-		WithWorkdir("/workspace").
-		WithFile("/workspace/.prototools", m.Dir.File(".prototools")).
-		// proto use
-		WithExec([]string{"proto", "use"}).
-		// rm .prototools
-		WithoutFile("/workspace/.prototools").
-		WithMountedDirectory("/workspace", source).
-		// moon setup
-		WithExec([]string{"moon", "setup"}).
-		// yarn install --immutable
-		WithExec([]string{"yarn", "install", "--immutable"})
-
-	m = m.setEnvs(ctx)
-
-	return m
-}
-
-func (m *WorkflowRuntime) WithAction(ctx context.Context, action string) *WorkflowRuntime {
-	m.Con = m.Con.WithExec([]string{"bash", "-l", "-c", fmt.Sprintf("./etc/scripts/actions/%s.ts", action)})
-
-	return m
-}
-
-func (m *WorkflowRuntime) Container(ctx context.Context) *dagger.Container {
-	return m.Con
-}
-
-func (m *WorkflowRuntime) Stdout(ctx context.Context) (string, error) {
-	stdout, err := m.Con.Stdout(ctx)
-	if err != nil {
-		return "", fmt.Errorf("failed to get stdout: %w", err)
-	}
-	return stdout, nil
-}
-
-func (m *WorkflowRuntime) setEnvs(ctx context.Context) *WorkflowRuntime {
-	m.Con = m.Con.
-		WithEnvVariable("MODE", m.Mode)
-
-	if m.GhToken != nil {
-		tokenString, _ := m.GhToken.Plaintext(ctx)
-
-		m.Con = m.Con.
-			WithEnvVariable("GH_TOKEN", tokenString)
-	}
-
-	return m
 }
