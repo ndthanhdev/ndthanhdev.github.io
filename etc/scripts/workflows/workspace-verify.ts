0939e27fa3714458a773af32e849d36eec4ac533
Duy Thanh Nguyen <16331812+ndthanhdev@users.noreply.github.com>
2026-04-20 16:20:50 +0700

    chore: update workflow files (#85)
    Co-authored-by: ndthanhdev <>

---
+++ etc/scripts/workflows/workspace-verify.ts
@@ -0,0 +1,25 @@
+#!/usr/bin/env -S yarn dlx tsx
+import "zx/globals";
+
+import { workDirs } from "../utils/work-dirs";
+
+$.verbose = true;
+
+cd(workDirs.etc.workflowRuntime.path);
+
+await $`${[
+	"dagger",
+	"call",
+	"--dir",
+	workDirs.path,
+	"build-env",
+	"with-env-variable",
+	"--name",
+	"MODE",
+	"--value",
+	"dev",
+	"with-moon-command",
+	"--command",
+	"ci",
+	"stdout",
+]}`;
